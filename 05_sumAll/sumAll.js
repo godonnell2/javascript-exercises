959922b19c7d85dcacee5a3ecae75456a994e295
Grace <graceodonnell2013@gmail.com>
2024-04-28 18:37:08 +0100

    Debug loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,19 @@
-const sumAll = function(a, b) {
-    if ( typeof a !== "number" || typeof b !== "number" ){
-        return 'ERROR';
-    }
-    let sumTotal = 0; 
-    let min = Math.min(a,b);
-    let max = Math.max(a,b);
- if (min < 0 || max < 0 ) {
-        return "ERROR";
-    } else { 
-            for (let i = min ; i <= max; i++){
-                sumTotal += i;
-            } return sumTotal;
-        } 
-}
-   
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  if (min < 0) {
+    return "ERROR";
+  }
+  let sumTotal = 0;
+  for (let i = min; i <= max; i++) {
+    sumTotal += i;
+  }
+  return sumTotal;
+};
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsius = (temp - 32) / (9 / 5);
+  let celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = (9 / 5) * temp + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
